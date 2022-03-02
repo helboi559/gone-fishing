b86c585f3df85082ffb863d8f73d53a6127724a9
Juan Ramirez <rami8224.2007@gmail.com>
2022-03-02 13:45:22 -0500

    completed assignment

--- gone-fishing.js
+++ gone-fishing.js
@@ -7,56 +7,55 @@
 
 const prompt = require('prompt-sync')({sigint:true});
 
-
-const randomFish = [{name:'nemo', weight: 0, cost: 2.50}, {name:'dory', weight:0, cost: 8.66},
-{name:'bruce', weight: 0, cost: 59.99}, {name:'jaws', weight:0, cost: 150000}];
-
-
 //set random fish caught by fisher
-const randomSelection = fishList => {
-    let randomIdx = Math.floor(Math.random() * fishList.length)
-    return fishList[randomIdx]
+const randomSelection = () => {
+    const randomFish = [{name:'nemo', weight: 0, cost: 0}, {name:'dory', weight:0, cost: 0},
+{name:'bruce', weight: 0, cost: 0}, {name:'jaws', weight:0, cost: 0}];
+    let randomIdx = Math.floor(Math.random() * randomFish.length)
+    return randomFish[randomIdx]
 }
 // console.log(randomFishWeight(randomFish))
 
 //***Fish for 1 hour***
-function fishingRounds(catches) {
+function fishingRounds() {
+    //set identify random fish to "catch"
+    const chalk = require('chalk');
     //running counts
     let weightTotal = 0;
     let priceTotal = 0;
     let roundTotal = 1;
     let keptTotal = [];
-    console.log('Youve entered a fishing tournament! You have 6 Hours to catch as many fish totaling under 10 lbs!')
+    //start the game
+    console.log(chalk.pink('Youve entered a fishing tournament! You have 6 Hours to catch as many fish totaling under 10 lbs!'))
+    //set limit at 6 hours
     while (roundTotal < 6) {
-        console.log(`the Time is ${roundTotal}:00 PM`)
-        let fishReeledIn = randomSelection(catches)
+        //display the current time
+        console.log(chalk.yellow(`the Time is ${roundTotal}:00 PM`))
         // console.log('random fish added', fishReeledIn)
-        //give random weight
-        fishReeledIn.weight = Math.random()* 10 + 1
+        //give random weight/random price
+        let fishReeledIn = randomSelection()
+        fishReeledIn.weight = Math.random()* 10 
+        fishReeledIn.cost = Math.random()* 100
         // console.log('random weight added',fishReeledIn)
-        //increase overall total's
-        // roundTotal = roundTotal += 1
-        console.log(`You caught a ${fishReeledIn.name}, weighing in at ${fishReeledIn.weight}, with a value of $${fishReeledIn.cost}`)
-        let caughtTotal = [];
-        caughtTotal.push(fishReeledIn);
+        //display what fish caught to user
+        console.log(chalk.green(`You caught a ${fishReeledIn.name}, weighing in at ${fishReeledIn.weight}, with a value of $${fishReeledIn.cost}`))
+        //add fish to 
+        // let caughtTotal = [];
+        // caughtTotal.push(fishReeledIn);
+        //if weight of current fish > total 
+        if(weightTotal + fishReeledIn.weight > 10){
+            // let overLimit = Number(prompt('You went over 10 lbs! Removing current fish: hit "1" to continue: '));
+            console.log(chalk.red('you went over 10 lbs! this fish will be released when asked'))  
+            roundTotal ++; 
+        } 
         
-        for(let i = 0 ; i < caughtTotal.length ; i++) {
-            if(caughtTotal[i].weight + weightTotal > 10) {
-                let overLimit = prompt('You went over 10 lbs! Removing current fish: hit "1" to continue: ');
-                if(overLimit === '1') {
-                    caughtTotal = caughtTotal.splice(i,1);
-
-                }
-            }
-        }
-        //ask user for input
-        let catchRelease = prompt('catch or release?("c", "r"): ')
+        let catchRelease = prompt('catch or release?("c", "r"): ');
         if(catchRelease === 'c') {
             weightTotal = weightTotal + fishReeledIn.weight
             priceTotal = priceTotal + fishReeledIn.cost
             keptTotal.push(fishReeledIn)
         } else if(catchRelease === 'r'){
-            caughtTotal = caughtTotal.splice(caughtTotal[caughtTotal.length-1], 1);
+            // caughtTotal = caughtTotal.splice(caughtTotal[caughtTotal.length-1], 1);
             console.log('Trew fish back in the sea!')
         } else {
             console.log('ERROR-INVALID ENTRY')
@@ -65,7 +64,9 @@ function fishingRounds(catches) {
         console.log(`So far you have caught: ${keptTotal.length} fish, ${weightTotal}lbs, $${priceTotal}`)
         
         //increase round by one
-        roundTotal ++;
+        roundTotal ++; 
+        
+        
     }
     console.log(`The time is 7 pm, times up!`)
     console.log(`You caught ${keptTotal.length} fish:`)
@@ -78,9 +79,9 @@ function fishingRounds(catches) {
     
 
 }
-console.log(fishingRounds(randomFish))
+console.log(fishingRounds())
 
-
+///----------
 // console.log(fishingRounds(randomFish))
 // console.log('overall total',weightTotal)
 // console.log('overall price',priceTotal)
@@ -95,7 +96,3 @@ console.log(fishingRounds(randomFish))
     //show progess & time
     //if max weight exceeds delete curr fish
         //prompt user for going over max
-
-
-
-
